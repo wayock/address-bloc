a981451dcc6ef99fcf16ac68dd9cbedc81e204d5
Matthew Wayock <wayock@gmail.com>
2019-06-01 17:49:00 -0400

    testing assignmnet completed

--- spec/menu_controller_spec.js
+++ spec/menu_controller_spec.js
@@ -15,3 +15,15 @@ const MenuController = require("../controllers/MenuController");
     });
    });
  });
+
+ describe("MenuController", () => {
+   beforeEach(() => {
+     this.menu = new MenuController();
+   });
+   describe("#remindMe()", () => {
+
+     it("should return 'Learning is a life-long pursuit'", () => {
+       expect(this.menu.remindMe()).toBe('Learning is a life-long pursuit');
+     });
+   });
+ });
