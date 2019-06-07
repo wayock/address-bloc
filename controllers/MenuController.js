6451b5421e5e4f752218f91f1c443135516bdb1c
Matthew Wayock <wayock@gmail.com>
2019-06-07 06:40:18 -0400

    list search checkpoint completed

--- controllers/MenuController.js
+++ controllers/MenuController.js
@@ -12,6 +12,7 @@ module.exports = class MenuController {
         choices: [
           "Add new contact",
           "View all contacts",
+          "Search for a contact",
           "Exit"
         ]
       }
@@ -29,6 +30,9 @@ module.exports = class MenuController {
            break;
          case "View all contacts":
            this.getContacts();
+           break;
+         case "Search for a contact":
+           this.search();
            break;
          case "Exit":
            this.exit();
@@ -77,6 +81,76 @@ module.exports = class MenuController {
           this.main();
         });
       }
+
+  search(){
+      inquirer.prompt(this.book.searchQuestions)
+      .then((target) => {
+       this.book.search(target.name)
+       .then((contact) => {
+          if(contact === null){
+            this.clear();
+            console.log("contact not found");
+            this.search();
+          } else {
+            this.showContact(contact);
+         }
+
+        });
+       })
+       .catch((err) => {
+         console.log(err);
+         this.main();
+       });
+      }
+
+      showContact(contact){
+        this._printContact(contact);
+          inquirer.prompt(this.book.showContactQuestions)
+            .then((answer) => {
+              switch(answer.selected){
+                case "Delete contact":
+                  this.delete(contact);
+                  break;
+                case "Main menu":
+                  this.main();
+                  break;
+                default:
+                  console.log("Something went wrong.");
+                  this.showContact(contact);
+              }
+            })
+            .catch((err) => {
+              console.log(err);
+              this.showContact(contact);
+            });
+      }
+
+      _printContact(contact){
+        console.log(`
+          name: ${contact.name}
+          phone number: ${contact.phone}
+          email: ${contact.email}
+          ---------------`
+        );
+    }
+
+    delete(contact){
+      inquirer.prompt(this.book.deleteConfirmQuestions)
+      .then((answer) => {
+        if(answer.confirmation){
+          this.book.delete(contact.id);
+          console.log("contact deleted!");
+          this.main();
+        } else {
+          console.log("contact not deleted");
+          this.showContact(contact);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+        this.main();
+      });
+    }
 
   exit(){
     console.log("Thanks for using AddressBloc!");
